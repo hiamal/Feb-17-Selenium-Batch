cfa713b540721a901eb0662b8c2e54f8e21330fd
Amal <hiamal@outlook.com>
2017-02-12 19:42:45 +0530

    Passing and Returning Parameters

--- src/TestCarrot.java
+++ src/TestCarrot.java
@@ -5,8 +5,8 @@ public class TestCarrot {
 		Carrot myCarrot = new Carrot();
 		
 		int hisPrice = myCarrot.marketPrice(10);
-		System.out.println("Salesman Price = " + hisPrice + hisPrice*10/100);
-
+		System.out.println("Salesman Price = " + (hisPrice + hisPrice*10/100));
+		
 	}
 
 }
